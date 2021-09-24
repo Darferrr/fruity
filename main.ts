a20e0e7f4fafbe569389716fd8f4d6f66f651db3
steveo <48230704+Darferrr@users.noreply.github.com>
2021-09-24 10:48:15 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let projectile: Sprite = null
 scene.setBackgroundImage(img`
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
@@ -140,3 +141,26 @@ let willis = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(willis)
 willis.setStayInScreen(true)
+info.setScore(0)
+info.startCountdown(10)
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . b b b b . . . . . . 
+        . . . . b b 3 3 3 3 b b . . . . 
+        . . . c b 3 3 3 3 1 1 b c . . . 
+        . . c b 3 3 3 3 3 1 1 1 b c . . 
+        . c c 1 1 1 3 3 3 3 1 1 3 c c . 
+        c c d 1 1 1 3 3 3 3 3 3 3 b c c 
+        c b d d 1 3 3 3 3 3 1 1 1 b b c 
+        c b b b 3 3 1 1 3 3 1 1 d d b c 
+        c b b b b d d 1 1 3 b d d d b c 
+        . c b b b b d d b b b b b b c . 
+        . . c c b b b b b b b b c c . . 
+        . . . . c c c c c c c c . . . . 
+        . . . . . . b 1 1 b . . . . . . 
+        . . . . . . b 1 1 b b . . . . . 
+        . . . . . b b d 1 1 b . . . . . 
+        . . . . . b d d 1 1 b . . . . . 
+        `, 50, 0)
+    projectile.setPosition(-4, 77)
+})
